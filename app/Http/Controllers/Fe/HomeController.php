a964ccf787238ff78719179b03ae0b7af9ff4e98
Manoj Vijayan <BEO\manoj.vijayan@act18.beo.in>
2020-01-28 08:44:24 +0530

    home

--- app/Http/Controllers/Fe/HomeController.php
+++ app/Http/Controllers/Fe/HomeController.php
@@ -35,7 +35,7 @@ class HomeController extends Controller
         $this->data['mainBanners']=Banner::where('status',1)->where('position_id','1')->get();  
 
         $this->data['upcomingEvents']=Event::where('status',1)->where('featured','1')->where('event_date','>=',$curDate)->get();
-        $this->data['zoneData']=DB::table('zones')->join('events', 'events.zone_id', '=', 'zones.id')->select('zones.title as zone','events.slug_url','events.image')->where('zones.status',1)->where('events.status',1)->where('events.identifier','zoneevent')->where('events.featured','1')->get();
+        $this->data['zoneData']=DB::table('zones')->join('events', 'events.zone_id', '=', 'zones.id')->select('zones.title as zone','events.slug_url','events.image')->where('zones.status','1')->where('events.status','1')->where('events.identifier','zoneevent')->where('events.featured','1')->get();
         $this->data['presidentData']=PresidentCorner::find(1);
 
         $this->setMetaData();
